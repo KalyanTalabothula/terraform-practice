6f3ac7286da65121946c1b08c0e1b306e67e8e78
kalyantalbothula <kalyan6166@gmail.com>
2025-08-07 11:55:46 +1000

    Updating

---
+++ variables/provider.tf
@@ -0,0 +1,31 @@
+
+# 🔍  terraform aws use provider 
+
+# 6.7.0 don't take current version, it might has any deffects so always try to take 2-3 versions backm means those are stable. that means even though already fixied the defects and created new version as well. and don't take beta versions as well, it is also like defected only. 
+
+# Provider okkati este saripotumda, Terraform aws ni authnticate avvali kada. miru provider echhesi connect avvamante yela .... So we need to give provider & Authentication also. 
+
+# Authentication is also done, just go to Gitbash give command: aws configure
+# it will core automatically if you configured, if not now give that keys. 
+
+# now go to terraform-practice and give command: terraform init
+# Output ela vastumdhi ( Terraform has been successfully initialized! ) 
+# ante intialize chestunnam, so adi aws ki sambamchina provider ni install chestumdhi ani. 
+# Succesfully instalized, Meee provider configuration check chesukoni, Internet lopaki velli e provider.exe anedhi techhukumtumdhi. that's it.
+
+# and .terraform.lock.hcl lo mee versions ni mention chestumdhi. 
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.98.0"
+    }
+  }
+}
+
+provider "aws" {      
+  # Configuration options  
+}
+
+
