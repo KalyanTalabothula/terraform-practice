adf661ee71cf690f0be18b3a2ca95afc566689b7
kalyantalbothula <kalyan6166@gmail.com>
2025-08-07 14:12:02 +1000

    Updating

---
+++ loops/route53.tf
@@ -0,0 +1,5 @@
+# before creating route53, we need some ec2 Private IP address. 
+# So, we need output of "aws_instance" "roboshop" so that we will create it.
+
+# For that we have syntax of output like variable syntax only. like piping 
+
