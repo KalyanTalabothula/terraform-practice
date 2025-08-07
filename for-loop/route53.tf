d75bb473257b0bc22d9a207d0b2aff1727f7f9ad
kalyantalbothula <kalyan6166@gmail.com>
2025-08-07 15:17:26 +1000

    Updating

--- for-loop/route53.tf
+++ for-loop/route53.tf
@@ -6,14 +6,14 @@
 # 🔎 aws route 53 terraform,  syntax will be there for route 53 
 
 
-# resource "aws_route53_record" "www" {
-#     count = 4
-#   zone_id = var.zone_id
-#   name    = "${var.instances[count.index]}.${var.domain_name}"  # "${mongodb}.${kalyanu.xyz}" 
-#   type    = "A"
-#   ttl     =  1 
-#   records = [aws_instance.roboshop[count.index].public_ip]
-# }
+resource "aws_route53_record" "www" {
+  for_each = aws_instance.roboshop
+  zone_id = var.zone_id
+  name    = "${each.key}.${var.domain_name}"  # "${mongodb}.${kalyanu.xyz}" 
+  type    = "A"
+  ttl     =  1 
+  records = [each.value.private_ip]
+}
 
 # line 12 : dot . is static that is not variable -->interpolation, we need mongodb.kalyanu.xyz right
 # So im getting the names from the ec2 instances tags right from there we are taking
