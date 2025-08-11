89d497eac5b886f92ebf5a317f5d150b898451c9
kalyantalbothula <kalyan6166@gmail.com>
2025-08-11 17:40:52 +1000

    updating

---
+++ state/provider.tf
@@ -0,0 +1,28 @@
+
+# 🔍  terraform remote state s3 bucket
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.98.0"
+    }
+  }
+
+# 🔍  terraform remote state s3 bucket
+
+  backend "s3" {        # <-- backend is ntg but state ani ardham.
+    bucket = "s3-kalyan-bucket"   # <---
+    key    = "remote-state-demo"    # <--- giving key as "remote-state-demo"
+    region = "us-east-1"
+    encrypt = true
+    use_lockfile = true 
+    #dynamodb_table = "84s-remote-state"  # <-- mention table name
+  }
+}
+
+provider "aws" {      
+  # Configuration options  
+}
+
+
